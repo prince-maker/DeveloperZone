38cb040b48793fbab559304c1fb28c679ee1d8e9
Guru <Guru@GuruBanakar>
2020-02-26 17:44:36 +0530

    added sout

--- src/main/java/net/codejava/AppMain.java
+++ src/main/java/net/codejava/AppMain.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppMain {
 	public static void main(String[] args) {
 		SpringApplication.run(AppMain.class, args);
+		System.out.println("Change");
 		System.out.println("****************************************************SUNIL************************************************************");
 	}
 	
